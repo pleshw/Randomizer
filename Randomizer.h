3ef440c9240314f6eaa9641ae2165147ac203b7e
Pleshw <pleshw@outlook.com>
2019-03-25 10:21:55 -0300

    New pattern

--- Randomizer.h
+++ Randomizer.h
@@ -19,44 +19,44 @@
 
 	private: 
 		// Cria um objeto para um dispositivo de randomização para gerar a seed se possível.
-		std::random_device _randomizador;
+		std::random_device _trueRandom;
 
 		// Cria um gerador que recebe uma função para retornar.
-		std::default_random_engine _gerador;
+		std::default_random_engine _generator;
 
 		// Guarda a seed que vai gerar os números retornados.
 		unsigned int _seed;
 
 		// Define o intervalo minimo e maximo a ser dado para a distribuição.
-		double _minimo;
-		double _maximo;
+		double _min;
+		double _max;
 
 		//Define o intervalo dos numéros aleatórios
-		std::uniform_real_distribution<double> _distribuicao;
+		std::uniform_real_distribution<double> _range;
 
 	public:
 		// Construtor com um range passado por parâmetro.
  		Randomizer( double min = 1.0 , double max = 6.0 ): 
-	 		_minimo  { min }, 
-	 		_maximo  { max+1 }, // Max inclusive
- 			_randomizador { /* void */ },
- 			_seed { _randomizador() },
- 			_gerador { _seed },
-	 		_distribuicao { _minimo, _maximo }
+	 		_min  { min }, 
+	 		_max  { max+1 }, // Max inclusive
+ 			_trueRandom { /* void */ },
+ 			_seed { _trueRandom() },
+ 			_generator { _seed },
+	 		_range { _min, _max }
 		{ /* Alocação e declarações extras se necessário. */ }
 
 
  		// Retorna um número real aleatório dentro do range.
- 		double GetRandom ( void ) { return _distribuicao(_gerador); }
+ 		double GetRandom ( void ) { return _range(_generator); }
 
  		// Retorna um número inteiro aleatório dentro do range.
- 		long GetRandomInt ( void ) { return std::floor( _distribuicao(_gerador) ); }
+ 		long GetRandomInt ( void ) { return std::floor( _range(_generator) ); }
 
  		// Retorna um array com o resultado do teste de n dados jogados aleatoriamente.
  		virtual Simulacao Test ( size_t n )
  		{
 			Simulacao tmp;
-			for( int i = _minimo; i < _maximo; i++ )
+			for( int i = _min; i < _max; i++ )
 		    	tmp[i] = 0;
 			for( int i = 0; i < n; i++ )
 		    	tmp[getRandomInt()]++;
